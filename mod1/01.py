1d1198b80dd5ffc0eafbee28fdb9c2ad1d3aa503
eeroh <eeroheli@metropolia.fi>
2025-08-19 14:37:09 +0300

    01

---
+++ mod1/01.py
@@ -0,0 +1,10 @@
+#kommentti
+print('Hei, Eero Helimäki!')
+
+#muuttuja
+nimi = 'matti'
+print(nimi)
+
+#tietoa käyttäjältä
+käyttäjä = input('Terve mikä on nimesi: ')
+print('Terve, ' + käyttäjä + '!')
