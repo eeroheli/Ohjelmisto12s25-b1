0cf0f075470f487294102567acf11c481551eb05
eeroh <eero.helimaki@metropolia.fi>
2025-08-26 14:13:45 +0300

    tunti

---
+++ mod3/03 tunti.py
@@ -0,0 +1,30 @@
+#
+import random
+
+satunnaisluku = random.randint(0,101)
+
+print(f'arvottu luku: {satunnaisluku}')
+# kolikonheittosimulaattori
+if satunnaisluku <= 50:
+    print("kruuna!")
+elif satunnaisluku > 50 <= 100:
+    print("klaava!")
+else:
+    print("kolikko jäi pystyyn!")
+
+#Dummy-kirjautuminen
+oikea_salasana = 'salakala'
+oikea_tunnus = 'eero'
+input_tunnus = input('anna tunnus: ')
+input_salasana = input('anna salasana: ')
+if input_salasana == oikea_salasana and input_tunnus == oikea_tunnus:
+    print('Tervetuloa!')
+    kehote = input('mitäs haluat tehdä? ')
+    if kehote == 'tervehtiä':
+        print('Terve!')
+    else:
+        print('en ymmärtänyt')
+else :
+    print('Väärä salasana!')
+
+print('ohjelma suljettu. ')
