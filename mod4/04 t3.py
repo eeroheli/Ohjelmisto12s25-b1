4379c306271bd3e8f2902aaf6f899f39d2d7c823
eeroh <eero.helimaki@metropolia.fi>
2025-08-27 13:37:35 +0300

    tunti

--- mod4/04 t3.py
+++ mod4/04 t3.py
@@ -2,11 +2,13 @@
 # kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi.
 # Lopuksi ohjelma tulostaa saaduista luvuista pienimmän ja suurimman.
 app_running = True
+numerot = []
 while app_running:
     luku = input('anna luku: ')
     if luku == '':
         app_running = False
     else :
         luku = float(luku)
+        numerot.append(luku)
 
-print(f'lukuesi suurin arvo on {max(luku)} ,ja pienin {min(luku)}.')
+print(f'lukuesi suurin arvo on {max(numerot)} ,ja pienin {min(numerot)}')
