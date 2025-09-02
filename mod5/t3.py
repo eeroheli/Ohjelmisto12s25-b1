76604704fa3c071bafe0697d4f6dca332dfa6a27
eeroh <eero.helimaki@metropolia.fi>
2025-09-02 09:54:59 +0300

    tunti

--- mod5/t3.py
+++ mod5/t3.py
@@ -3,4 +3,12 @@
 #Esimerkiksi luku 13 on alkuluku, koska se voidaan jakaa vain luvuilla 1 ja 13 siten, että jako menee tasan.
 #Toisaalta esimerkiksi luku 21 ei ole alkuluku, koska se voidaan jakaa tasan myös luvulla 3 tai luvulla 7.
 
-luku = input("Annna kokonaisluku: ")
+luku = float(input("Annna kokonaisluku: "))
+itsellaan = luku % luku
+yhedella = luku % 1
+kahdella = luku % 2
+kolmella = luku % 3
+if yhedella == 0 and itsellaan == 0 and kahdella != 0 and kolmella != 0:
+    print(f'luku {luku:.0f} on alkuluku')
+else :
+    print(f'luku {luku:.0f} ei ole alkuluku')
--- mod5/t4.py
+++ mod5/t4.py
@@ -3,3 +3,9 @@
 # Lopuksi ohjelma tulostaa kaupunkien nimet yksi kerrallaan allekkain samassa järjestyksessä kuin ne syötettiin.
 # käytä for-toistorakennetta nimien kysymiseen ja for/in toistorakennetta niiden läpikäymiseen.
 
+kaupingit = []
+for i in range(5):
+    kaupunki = input('anna kaupunki: ')
+    kaupingit.append(kaupunki)
+for i1 in kaupingit:
+    print(i1)
