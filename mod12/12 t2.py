edae034550b08d32f08494ecc251f4067a27c421
eeroh <eero.helimaki@metropolia.fi>
2025-10-28 11:17:12 +0200

    tunti

---
+++ mod12/12 t2.py
@@ -0,0 +1,15 @@
+# 2. Tutustu avoimeen OpenWeather-säärajapintaan: https://openweathermap.org/api.
+# Kirjoita ohjelma, joka kysyy käyttäjältä paikkakunnan nimen ja tulostaa sitä vastaavan säätilan tekstin sekä lämpötilan Celsius-asteina.
+# Perehdy rajapinnan dokumentaatioon riittävästi. Palveluun rekisteröityminen on tarpeen, jotta saat rajapintapyynnöissä tarvittavan API-avaimen (API key).
+# Selvitä myös, miten saat Kelvin-asteet muunnettua Celsius-asteiksi.
+import requests
+import json
+paikkakunta = input('anna paikkakunnan nimi: ')
+pyynto1 = f'http://api.openweathermap.org/geo/1.0/direct?q={paikkakunta}&limit=5&appid=26e95cc1408994e6db89afaf8ef94370'
+vastaus1 = requests.get(pyynto1).json()
+pyynto2 = f'https://api.openweathermap.org/data/2.5/weather?lat={vastaus1[0]["lat"]}&lon={vastaus1[0]["lon"]}&appid=26e95cc1408994e6db89afaf8ef94370'
+vastaus2 = requests.get(pyynto2).json()
+
+
+saa = vastaus2['main']['temp'] - 273.15
+print(f'{paikkakunta} paikkakunnalla on {saa:.2f} astetta lämmintä')
