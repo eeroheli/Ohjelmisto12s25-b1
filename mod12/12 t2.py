47080ba460ab4a4bf95227dcce1017a81d76c48d
eeroh <eero.helimaki@metropolia.fi>
2025-10-28 11:39:36 +0200

    tunti

--- mod12/12 t2.py
+++ mod12/12 t2.py
@@ -7,9 +7,9 @@ import json
 paikkakunta = input('anna paikkakunnan nimi: ')
 pyynto1 = f'http://api.openweathermap.org/geo/1.0/direct?q={paikkakunta}&limit=5&appid=26e95cc1408994e6db89afaf8ef94370'
 vastaus1 = requests.get(pyynto1).json()
-pyynto2 = f'https://api.openweathermap.org/data/2.5/weather?lat={vastaus1[0]["lat"]}&lon={vastaus1[0]["lon"]}&appid=26e95cc1408994e6db89afaf8ef94370'
+pyynto2 = f'https://api.openweathermap.org/data/2.5/weather?lat={vastaus1[0]["lat"]}&lon={vastaus1[0]["lon"]}&appid=26e95cc1408994e6db89afaf8ef94370&units=metric&lang=fi'
 vastaus2 = requests.get(pyynto2).json()
 
 
 saa = vastaus2['main']['temp'] - 273.15
-print(f'{paikkakunta} paikkakunnalla on {saa:.2f} astetta lämmintä')
+print(f'{paikkakunta} paikkakunnalla on {vastaus2['main']['temp']:.2f} astetta lämmintä ja säätä kuvaillaan: {vastaus2['weather'][0]['description']}')
